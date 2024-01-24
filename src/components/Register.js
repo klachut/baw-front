3b886a8556e31608d98347f802b588246f5bdcc2
Parrot User <user@localhost.localdomain>
2024-01-24 02:56:22 -0500

    register

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,22 +3,30 @@ import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 
 const Register = () => {
-    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
     const [passwordOne, setPasswordOne] = useState('');
     const [passwordTwo, setPasswordTwo] = useState('');
     const history = useNavigate();
-  
-    const handleRegister = async () => {
+
+    const handleLogin = async () => {
       try {
-        // Wysyłanie danych logowania do API
-        const response = await axios.post('URL_DO_LOGOWANIA', { email, passwordOne });
-  
-        // Przekierowanie po udanym logowaniu
-        history.push('/articles');
+        const response = await fetch('http://localhost:3001/api/auth/register', {
+          method: "POST",
+          body: JSON.stringify({
+              login: username,
+              password: passwordOne,
+          }),
+          headers: {"Content-Type": "application/json"},
+          credentials: "include"
+      });
+        await login();
+        navigate('/watki')
+
       } catch (error) {
-        console.error('Błąd logowania', error);
-      }
-    };
+    }
+  };
+
+
   
     return (
        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -36,18 +44,18 @@ const Register = () => {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
               </label>
               <div className="mt-2">
                 <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
+                  id="username"
+                  name="username"
+                  type="username"
+                  autoComplete="username"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  value={email} onChange={(e) => setEmail(e.target.value)} 
+                  value={username} onChange={(e) => setUsername(e.target.value)} 
                 />
               </div>
             </div>
